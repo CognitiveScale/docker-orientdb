0437bab7c8a26c42e4855f2136538825b2b7736c
Ubuntu <ubuntu@ip-10-0-100-176.ec2.internal>
2015-01-06 18:01:41 +0000

    Added GratefulDeadConcerts

---
+++ GratefulDeadConcerts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+database_dir=/opt/orientdb/databases
+
+if [ -d "${database_dir}/GratefulDeadConcerts" ];then
+  echo "GratefulDeadConcerts database found."
+else
+  echo "installing GratefulDeadConcerts"
+/opt/orientdb/bin/console.sh <(echo 'connect remote:localhost root root ; create database remote:localhost/GratefulDeadConcerts root root plocal; import database /tmp/GratefulDeadConcerts.gz')
+fi
